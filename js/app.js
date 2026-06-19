b9c063a46f11f826e83d259a0665e5e6330765c2
alan-desing <alanbissio@gmail.com>
2026-06-19 02:46:52 -0300

    manifest y service-worker

---
+++ js/app.js
@@ -0,0 +1,15 @@
+if ("serviceWorker" in navigator) {
+
+    window.addEventListener("load", () => {
+
+        navigator.serviceWorker.register("service-worker.js")
+            .then(() => {
+                console.log("Service Worker registrado.");
+            })
+            .catch(error => {
+                console.error("Error al registrar Service Worker:", error);
+            });
+
+    });
+
+}
---
+++ service-worker.js
@@ -0,0 +1,78 @@
+const CACHE_NAME = "relevamiento-v1";
+
+const ARCHIVOS_CACHE = [
+    "/",
+    "/index.html",
+
+    "/css/estilos.css",
+
+    "/js/app.js",
+    "/js/camara.js",
+    "/js/ocr.js",
+    "/js/ubicacion.js",
+    "/js/copiar.js",
+
+    "/manifest/manifest.json",
+
+    "/assets/iconos/icono-192.jpeg",
+    "/assets/iconos/icono-512.jpeg"
+];
+
+
+// Instalar Service Worker
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(ARCHIVOS_CACHE);
+            })
+
+    );
+
+});
+
+
+// Activar Service Worker
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys()
+            .then(cacheNames => {
+
+                return Promise.all(
+
+                    cacheNames.map(cache => {
+
+                        if (cache !== CACHE_NAME) {
+                            return caches.delete(cache);
+                        }
+
+                    })
+
+                );
+
+            })
+
+    );
+
+});
+
+
+// Interceptar peticiones
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(response => {
+
+                return response || fetch(event.request);
+
+            })
+
+    );
+
+});
